f19f9a6a47bcade8b2525ae862f3686cfba41dcf
FrancoRosa <km115.franco@gmail.com>
2021-11-13 22:59:20 -0500

    Recover files from old project

---
+++ compile.sh
@@ -0,0 +1,28 @@
+# st-flash write generic_boot20_pc13.bin 0x08000000
+start_time=$(date +%s.%3N)
+
+echo ''
+echo '... compile'
+echo ''
+
+arduino-cli compile \
+--fqbn STMicroelectronics:stm32:GenF1:pnum=BLUEPILL_F103C6,\
+upload_method=swdMethod,xserial=generic,usb=CDCgen,xusb=FS,opt=osstd,dbg=none,rtlib=nano \
+stm32 --build-path build  
+
+compile_time=$(date +%s.%3N)
+elapsed=$(echo "scale=3; $compile_time - $start_time" | bc)
+echo '... compiled, elapsed time:' $elapsed 'ms'
+
+echo ''
+echo '... upload  '
+echo ''
+
+sudo st-flash --hot-plug write build/stm32.ino.bin 0x08000000 
+sudo st-flash reset 
+
+end_time=$(date +%s.%3N)
+elapsed=$(echo "scale=3; $end_time - $start_time" | bc)
+echo ''
+echo '... done, elapsed time:' $elapsed 'ms'
+echo ''
